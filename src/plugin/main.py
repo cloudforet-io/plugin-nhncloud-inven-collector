ba164eb2220d7edc006da8e02166da6a4c8e82f4
junho100 <bemodesty306@naver.com>
2024-07-05 23:24:34 +0900

    feat: Add base logic
    - Add BaseManager, BaseCollector for common base logic
    - Add logic calls Managers according to auth(schema) type
    
    Signed-off-by: Junho Baek <bemodesty306@naver.com>

--- src/plugin/main.py
+++ src/plugin/main.py
@@ -1,8 +1,11 @@
+import logging
 from typing import Generator
 from spaceone.inventory.plugin.collector.lib.server import CollectorPluginServer
-from .manager.resource_manager import ResourceManager
+from .manager.base import NHNCloudBaseManager
 
 app = CollectorPluginServer()
+
+_LOGGER = logging.getLogger("cloudforet")
 
 
 @app.route("Collector.init")
@@ -14,11 +17,25 @@ def collector_verify(params: dict) -> None:
     # Verify connector using secret
     pass
 
+
+def _check_secret_data(secret_data):
+    pass
+
+
+def _check_schema(schema):
+    pass
+
+
 @app.route("Collector.collect")
 def collector_collect(params: dict) -> Generator[dict, None, None]:
-    options = params["options"]
-    secret_data = params["secret_data"]
-    schema = params.get("schema")
+    secret_data = params.get("secret_data", {})
+    schema = params.get("schema", "")
 
-    resource_mgr = ResourceManager()
-    return resource_mgr.collect_resources(options, secret_data, schema)
+    _check_secret_data(secret_data)
+    _check_schema(schema)
+
+    # Temporal code. This will be deleted when local testing using schema properties becomes possible.
+    auth_type = NHNCloudBaseManager.get_auth_type_by_secret_data(secret_data)
+
+    for manager in NHNCloudBaseManager.list_managers_by_schema(auth_type.value):
+        yield from manager().collect_resources(secret_data)
