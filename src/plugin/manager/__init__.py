7b3adfdbe2787a2bd0c20cf3598724a7eedc291b
junho100 <bemodesty306@naver.com>
2024-07-11 16:21:04 +0900

    fix: add missing import statements
    Signed-off-by: junho100 <bemodesty306@naver.com>

--- src/plugin/manager/__init__.py
+++ src/plugin/manager/__init__.py
@@ -1,1 +1,4 @@
 from plugin.manager.email.category_manager import CategoryManager
+from plugin.manager.email.tag_manager import TagManager
+from plugin.manager.email.template_manager import TemplateManager
+from plugin.manager.api_gateway.service_manager import APIGatewayServiceManager
