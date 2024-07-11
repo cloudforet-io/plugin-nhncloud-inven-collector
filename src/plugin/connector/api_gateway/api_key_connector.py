326b4ac8e8963f8c81adb66097da6158cf964921
junho100 <bemodesty306@naver.com>
2024-07-11 17:48:38 +0900

    feat: add api gateway api key resource
    Signed-off-by: junho100 <bemodesty306@naver.com>

---
+++ src/plugin/connector/api_gateway/api_key_connector.py
@@ -0,0 +1,34 @@
+import logging
+
+import requests
+
+from plugin.conf.cloud_service_conf import REGION
+from plugin.connector.base import NHNCloudBaseConnector
+
+_LOGGER = logging.getLogger("cloudforet")
+
+class APIKeyConnector(NHNCloudBaseConnector):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @staticmethod
+    def list_keys(app_key, region:REGION) -> list:
+        keys = []
+        idx = 1
+        while True:
+            response = requests.get(f"https://{region.name.lower()}-apigateway.api.nhncloudservice.com/v1.0/appkeys/{app_key}/apikeys?page={idx}",
+                                    headers={
+                                        "Content-Type": "application/json",
+                                    })
+
+            if response.status_code != 200 or response.json().get('header').get('isSuccessful') is False:
+                _LOGGER.error(f"Failed to get keys. {response.json()}")
+                raise Exception(f"Failed to get keys. {response.json()}")
+
+            if not response.json()['apiKeyList']:
+                break
+
+            keys.extend(response.json()['apiKeyList'])
+            idx += 1
+
+        return keys
--- src/plugin/manager/__init__.py
+++ src/plugin/manager/__init__.py
@@ -2,3 +2,4 @@ from plugin.manager.email.category_manager import CategoryManager
 from plugin.manager.email.tag_manager import TagManager
 from plugin.manager.email.template_manager import TemplateManager
 from plugin.manager.api_gateway.service_manager import APIGatewayServiceManager
+from plugin.manager.api_gateway.api_key_manager import APIKeyManager
