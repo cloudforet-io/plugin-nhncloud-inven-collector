718e4b7c867d7fca09014bcf9a52811245496949
junho100 <bemodesty306@naver.com>
2024-07-11 13:19:04 +0900

    feat: add email template plugin
    Signed-off-by: junho100 <bemodesty306@naver.com>

---
+++ src/plugin/connector/email/template_connector.py
@@ -0,0 +1,37 @@
+import logging
+import requests
+
+from plugin.connector.base import NHNCloudBaseConnector
+
+_LOGGER = logging.getLogger("cloudforet")
+
+
+class TemplateConnector(NHNCloudBaseConnector):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @staticmethod
+    def list_templates(app_key, secret_key) -> list:
+        templates = []
+        idx = 1
+        while True:
+            response = requests.get(
+                f"https://email.api.nhncloudservice.com/email/v22.1/appKeys/{app_key}/templates?pageNum={idx}",
+                headers={
+                    "Content-Type": "application/json",
+                    "X-Secret-Key": secret_key
+                })
+
+            if response.json().get('header').get('isSuccessful') is False:
+                _LOGGER.error(f"Failed to get templates. {response.json()}")
+                raise Exception(f"Failed to get templates. {response.json()}")
+
+
+            if not response.json()['body']['data']:
+                break
+
+            templates.extend(response.json()['body']['data'])
+            idx += 1
+
+        return templates
---
+++ src/plugin/manager/email/template_manager.py
@@ -0,0 +1,54 @@
+import logging
+from spaceone.inventory.plugin.collector.lib import *
+
+from plugin.conf.cloud_service_conf import AUTH_TYPE, ASSET_URL
+from plugin.connector.email.template_connector import TemplateConnector
+from plugin.manager.base import NHNCloudBaseManager
+
+_LOGGER = logging.getLogger("cloudforet")
+
+
+class TemplateManager(NHNCloudBaseManager):
+    auth_type = AUTH_TYPE.APP_KEY
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cloud_service_group = "Email"
+        self.cloud_service_type = "Template"
+        self.metadata_path = f"metadata/{self.cloud_service_group.lower()}/{self.cloud_service_type.lower()}.yaml"
+
+    def create_cloud_service_type(self):
+        cloud_service_type = make_cloud_service_type(
+            name=self.cloud_service_type,
+            group=self.cloud_service_group,
+            provider=self.provider,
+            metadata_path=self.metadata_path,
+            is_primary=False,
+            is_major=False,
+            tags={
+                "spaceone:icon": f"{ASSET_URL}/email.png"
+            }
+        )
+
+        return cloud_service_type
+
+    def create_cloud_service(self, secret_data):
+        template_connector = TemplateConnector()
+        templates = template_connector.list_templates(secret_data.get("app_key"), secret_data.get("email_secret_key"))
+
+        for template in templates:
+            reference = {
+                    "resource_id": template.get("templateId"),
+                    "external_link": ""
+                    }
+            cloud_service = make_cloud_service(
+                name=template["templateName"],
+                cloud_service_type=self.cloud_service_type,
+                cloud_service_group=self.cloud_service_group,
+                provider=self.provider,
+                data=template,
+                account=secret_data.get("project_id"),
+                reference=reference,
+            )
+
+            yield cloud_service
