153a746365925722b9ec06774f385483c5b74b9b
junho100 <bemodesty306@naver.com>
2024-07-13 16:01:04 +0900

    feat: add push token resource
    Signed-off-by: junho100 <bemodesty306@naver.com>

--- src/plugin/manager/__init__.py
+++ src/plugin/manager/__init__.py
@@ -4,3 +4,4 @@ from plugin.manager.email.template_manager import TemplateManager
 from plugin.manager.api_gateway.service_manager import APIGatewayServiceManager
 from plugin.manager.api_gateway.api_key_manager import APIKeyManager
 from plugin.manager.api_gateway.usage_plan_manager import UsagePlanManager
+from plugin.manager.push.token_manager import PushTokenManager
---
+++ src/plugin/manager/push/token_manager.py
@@ -0,0 +1,55 @@
+import logging
+from spaceone.inventory.plugin.collector.lib import *
+
+from plugin.conf.cloud_service_conf import AUTH_TYPE, ASSET_URL
+from plugin.connector.push.token_connector import PushTokenConnector
+from plugin.manager.base import NHNCloudBaseManager
+
+_LOGGER = logging.getLogger("cloudforet")
+
+
+class PushTokenManager(NHNCloudBaseManager):
+    auth_type = AUTH_TYPE.APP_KEY
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cloud_service_group = "Push"
+        self.cloud_service_type = "Token"
+        self.metadata_path = f"metadata/{self.cloud_service_group.lower()}/{self.cloud_service_type.lower()}.yaml"
+
+    def create_cloud_service_type(self):
+        cloud_service_type = make_cloud_service_type(
+            name=self.cloud_service_type,
+            group=self.cloud_service_group,
+            provider=self.provider,
+            metadata_path=self.metadata_path,
+            is_primary=True,
+            is_major=True,
+            tags={
+                "spaceone:icon": f"{ASSET_URL}/push.png"
+            }
+        )
+
+        return cloud_service_type
+
+    def create_cloud_service(self, secret_data):
+        token_connector = PushTokenConnector()
+        tokens = token_connector.list_tokens(secret_data.get("app_key"), secret_data.get("push_secret_key"))
+
+        for token in tokens:
+            reference = {
+                    "resource_id": token.get("token"),
+                    "external_link": ""
+                    }
+
+            cloud_service = make_cloud_service(
+                name=token["uid"],
+                cloud_service_type=self.cloud_service_type,
+                cloud_service_group=self.cloud_service_group,
+                provider=self.provider,
+                data=token,
+                account=secret_data.get("project_id"),
+                reference=reference,
+            )
+
+            yield cloud_service
