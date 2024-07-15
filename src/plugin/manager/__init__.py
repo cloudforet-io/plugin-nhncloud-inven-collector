6f5ac46057beae4dbfbf8fb403c03ed2100ccdf1
junho100 <bemodesty306@naver.com>
2024-07-15 18:15:35 +0900

    feat: add transit hub resource
    Signed-off-by: junho100 <bemodesty306@naver.com>

--- src/plugin/manager/__init__.py
+++ src/plugin/manager/__init__.py
@@ -6,3 +6,4 @@ from plugin.manager.api_gateway.api_key_manager import APIKeyManager
 from plugin.manager.api_gateway.usage_plan_manager import UsagePlanManager
 from plugin.manager.push.token_manager import PushTokenManager
 from plugin.manager.push.tag_manager import PushTagManager
+from plugin.manager.transit_hub.transit_hub_manager import TransitHubManager
