f1547ab6f8e76fc1c9910f559be8f01df506a11d
in-seo <sis000512@gmail.com>
2024-07-19 15:39:01 +0900

    feat: Security Group (inquiry, Rule)
    Signed-off-by: in-seo <sis000512@gmail.com>

---
+++ src/plugin/connector/sg/security_groups_connector.py
@@ -0,0 +1,28 @@
+import logging
+
+import requests
+
+from plugin.conf.cloud_service_conf import REGION
+from plugin.connector.base import NHNCloudBaseConnector
+
+_LOGGER = logging.getLogger("cloudforet")
+
+
+class SGConnector(NHNCloudBaseConnector):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_security_groups(self, secret_data: dict, region: REGION) -> dict:
+        token = self.get_token(secret_data)
+
+        url = f"https://{region.name.lower()}-api-network-infrastructure.nhncloudservice.com/v2.0/security-groups?tenant_id={secret_data.get('tenant_id')}"
+        headers = {"X-Auth-Token": token}
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code != 200:
+            _LOGGER.error(f"Failed to get Security Groups. {response.json()}")
+            raise Exception(f"Failed to get Security Groups. {response.json()}")
+
+        return response.json().get("security_groups", [])
