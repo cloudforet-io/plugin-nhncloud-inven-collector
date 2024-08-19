9deaafec487dd532cc348203c406acd5ce1e6806
junho100 <bemodesty306@naver.com>
2024-08-19 18:36:56 +0900

    feat: add api gw exception handling
    Signed-off-by: junho100 <bemodesty306@naver.com>

--- src/plugin/connector/api_gateway/api_key_connector.py
+++ src/plugin/connector/api_gateway/api_key_connector.py
@@ -22,6 +22,9 @@ class APIKeyConnector(NHNCloudBaseConnector):
                                     })
 
             if response.status_code != 200 or response.json().get('header').get('isSuccessful') is False:
+                # Ignore collecting request when the service is inactivated. This logic does not ensure that app key is valid.
+                if response.json().get('header').get('resultCode') == 401199000:
+                    return []
                 _LOGGER.error(f"Failed to get keys. {response.json()}")
                 raise Exception(f"Failed to get keys. {response.json()}")
 
